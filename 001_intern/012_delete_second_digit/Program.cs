f9aadf7748f4a88321ecbcf6b30d28b79663335e
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 20:29:48 +0500

    ex 012 done

---
+++ 001_intern/012_delete_second_digit/Program.cs
@@ -0,0 +1,14 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное целое число");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number/100 < 1 ^ number/100 > 9)
+{
+    Console.WriteLine("Число не трехзначное");
+    return;
+}
+
+number = number/100*10 + number%10;
+Console.WriteLine("Число без второй цифры " + number);
