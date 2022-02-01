3466c87384ce1b3eabef49246c2902407e9deb4c
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 18:01:48 +0500

    ex 009 done

---
+++ 001_intern/009_last_of_three_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное целое число");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number/100 < 1 ^ number/100 > 9)
+{
+    Console.WriteLine("Число не трехзначное");
+    return;
+}
+
+Console.WriteLine("Последняя цифра числа " + number%10);
