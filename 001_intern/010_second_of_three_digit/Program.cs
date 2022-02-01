13f421839daa6e447b960b6078080aff1b96f6e2
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 18:07:48 +0500

    ex 010 done

---
+++ 001_intern/010_second_of_three_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное целое число");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number/100 < 1 ^ number/100 > 9)
+{
+    Console.WriteLine("Число не трехзначное");
+    return;
+}
+
+Console.WriteLine("Вторая цифра числа " + number/10%10);
