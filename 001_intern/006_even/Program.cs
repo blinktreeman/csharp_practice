789a5306cd6a59387c93078db7bd2c4be392982c
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 17:18:59 +0500

    ex 006 done

---
+++ 001_intern/006_even/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine("Число " + number + " четное");
+else Console.WriteLine("Число " + number + " нечетное");
