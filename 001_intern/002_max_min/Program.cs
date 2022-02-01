684e084382f87bb3a342cd0019b219ddc94bb21d
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 15:12:01 +0500

    ex 002 done

---
+++ 001_intern/002_max_min/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа");
+
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+
+if (numA > numB) Console.WriteLine("Большее число " + numA + " и меньшее " + numB);
+else 
+{
+    if (numA < numB) Console.WriteLine("Большее число " + numB + " и меньшее " + numA);
+    else Console.WriteLine("Числа равны");
+}
