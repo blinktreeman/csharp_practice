5346e372e1fd202d0cc3e7a841141d5fc21a70bb
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 20:13:37 +0500

    ex 011 done

--- 001_intern/011_max_digit/Program.cs
+++ 001_intern/011_max_digit/Program.cs
@@ -1,5 +1,8 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число в интервале от 10 до 99");
 
+int number = int.Parse(Console.ReadLine());
 
+if (number/10 > number%10) Console.WriteLine("Большее значение " + number/10);
+else Console.WriteLine("Большее значение " + number%10);
