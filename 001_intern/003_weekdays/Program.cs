b3026ff6787bdbf7a13289edbcd7416f3a8d9c78
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 15:37:53 +0500

    ex 003 done

---
+++ 001_intern/003_weekdays/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] weekDays = new string[] {"Понедельник",
+                                    "Вторник",
+                                    "Среда",
+                                    "Четверг",
+                                    "Пятница",
+                                    "Суббота",
+                                    "Воскресенье"};
+
+Console.WriteLine("Введите номер дня в неделе");
+
+int dayNum = int.Parse(Console.ReadLine());
+
+if (dayNum > 0 & dayNum < 8) Console.WriteLine("День недели под номером " + dayNum + " это " + weekDays[dayNum-1]);
+else Console.WriteLine("Такого дня не бывает");
