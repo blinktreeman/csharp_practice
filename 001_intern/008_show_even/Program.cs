65107909fc1a6a1312d3f7a903adcb018cac5ef8
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 17:43:42 +0500

    ex 008 done

---
+++ 001_intern/008_show_even/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число N");
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Четные числа в диапазоне от 1 до N");
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0) Console.Write(i + " ");
+}
