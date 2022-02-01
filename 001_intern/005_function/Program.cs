831272b6661308e6edfccfc05bb80582e4144f12
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 17:10:42 +0500

    ex 005 done

---
+++ 001_intern/005_function/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+int faMethod(int a)
+{
+    int y = a * 2;  // some function y = f(a)
+    return y;
+}
+
+Console.WriteLine("Для вычисления значения y = f(a) введите значение аргумента 'a'");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Значение функции равно " + faMethod(numA));
