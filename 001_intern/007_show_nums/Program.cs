299599929e0bbd803babaf163fba9ffc1f8a46e3
Eugene Elantsev <eugene_el@mail.ru>
2022-02-01 17:27:23 +0500

    ex 007 done

---
+++ 001_intern/007_show_nums/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите число N");
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Числа от -N до N");
+for (int i = number*(-1); i <= number; i++) 
+{
+    Console.Write(i + " ");
+}
