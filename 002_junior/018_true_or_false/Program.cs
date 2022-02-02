e0de9d675e60d8adf4a203e661cc3ebe585d004e
Eugene Elantsev <eugene_el@mail.ru>
2022-02-02 14:53:31 +0500

    ex 18 done

---
+++ 002_junior/018_true_or_false/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool[,] variantsArr = new bool[4, 2]{{true, true}, {true, false}, {false, true}, {false, false}};
+
+Console.WriteLine("Таблица истинности");
+Console.WriteLine("|   X   |   Y   | ¬(X ⋁ Y) = ¬X ⋀ ¬Y  |");
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("| " + Convert.ToString(variantsArr[i, 0]) + " | " + Convert.ToString(variantsArr[i, 1]) + " | " + 
+        Convert.ToString(!(variantsArr[i, 0] & variantsArr[i, 1]) == !variantsArr[i, 0] | !variantsArr[i, 1]));
+}
