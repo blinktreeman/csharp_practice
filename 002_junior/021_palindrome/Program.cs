9a8e0200ceb12cf1ce9ad442b175be2963ede2e4
Eugene Elantsev <eugene_el@mail.ru>
2022-02-02 18:02:54 +0500

    ex 021 done

---
+++ 002_junior/021_palindrome/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа проверяет является ли пятизначное число палиндромом
+
+Console.WriteLine("Введите пятизнаяное целое число");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number < 10000 | number > 99999)
+{
+    Console.WriteLine("Число не пятизначное");
+    return;
+}
+
+if (number / 10000 == number % 10 & number / 1000 % 10 == number / 10 % 10)
+{
+    Console.WriteLine("Число " + number + " является палиндромом");
+}
+else Console.WriteLine("Число " + number + " не является палиндромом");
