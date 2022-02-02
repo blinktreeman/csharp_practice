1f940ade693ab83c25a4c31b433dd084db87d5fa
Eugene Elantsev <eugene_el@mail.ru>
2022-02-02 10:10:16 +0500

    ex 013 done

---
+++ 001_intern/013_is_multiple_or_not/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+Console.WriteLine("Введите целое число (делимое)");
+int dividend = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите целое число (делитель)");
+int divisor = int.Parse(Console.ReadLine());
+
+if (dividend % divisor == 0) Console.WriteLine("Число " + dividend + " кратно числу " + divisor);
+else 
+{
+    Console.WriteLine("Число " + dividend + " не кратно числу " + divisor);
+    Console.WriteLine("остаток от деления равен " + dividend % divisor);
+}
