637e32d22abd9c963becf017f77f6d53f4ec12de
Eugene Elantsev <eugene_el@mail.ru>
2022-02-02 17:28:56 +0500

    ex 020 done

---
+++ 002_junior/020_show_range/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти координатной плоскости");
+int quarterNum = int.Parse(Console.ReadLine());
+
+string xRange = "", yRange = "";
+
+switch (quarterNum)
+{
+    case 1:
+        xRange = "(0; +oo)";
+        yRange = "(0; +oo)";
+        break;
+    case 2:
+        xRange = "(0; -oo)";
+        yRange = "(0; +oo)";
+        break;
+    case 3:
+        xRange = "(0; -oo)";
+        yRange = "(0; -oo)";
+        break;
+    case 4:
+        xRange = "(0; +oo)";
+        yRange = "(0; -oo)";
+        break;
+}
+
+Console.WriteLine("Диапазоны координат для четверти " + quarterNum);
+Console.WriteLine("по X " + xRange);
+Console.WriteLine("по Y " + yRange);
