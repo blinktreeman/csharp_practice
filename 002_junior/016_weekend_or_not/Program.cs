901a39caa04411a0733447511ab6ed793d8dcbc5
Eugene Elantsev <eugene_el@mail.ru>
2022-02-02 11:09:17 +0500

    ex 016 done

---
+++ 002_junior/016_weekend_or_not/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите номер дня недели");
+
+int weekDayNum = int.Parse(Console.ReadLine());
+
+if (weekDayNum == 6 ^ weekDayNum == 7) Console.WriteLine("День недели с номером " + weekDayNum + " выходной");
+else Console.WriteLine("День недели с номером " + weekDayNum + " не выходной");
