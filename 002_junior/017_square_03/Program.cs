7786631d1e588c9366e2366b536caf5663df4bd9
Eugene Elantsev <eugene_el@mail.ru>
2022-02-02 15:55:20 +0500

    ex 017 done

---
+++ 002_junior/017_square_03/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Введите два целых числа");
+
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+
+if (numA == numB * numB | numB == numA * numA)
+{
+    if (numA == numB * numB) Console.WriteLine("Число " + numA + " есть квадрат числа " + numB);
+    else Console.WriteLine("Число " + numB + " есть квадрат числа " + numA);
+    return;
+}
+Console.WriteLine("Одно число не является квадратом другого");
