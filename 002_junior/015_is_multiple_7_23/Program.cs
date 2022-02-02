440fe169042683566a107039be484569f5648ddf
Eugene Elantsev <eugene_el@mail.ru>
2022-02-02 11:01:02 +0500

    ex 015 done

---
+++ 002_junior/015_is_multiple_7_23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("Введите целое число");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 7 == 0 & number % 23 == 0) Console.WriteLine("Число кратно 7-ми и 23-ем");
+else Console.WriteLine("Число не кратно 7-ми или 23-ем");
