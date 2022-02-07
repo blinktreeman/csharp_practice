fcdad0da0534de78ac690574eb86422d37cf5dff
Eugene <eugene_el@mail.ru>
2022-02-07 21:19:56 +0700

    ex 032 done

---
+++ 003_senior/032_array_8_01/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int[] arrayA = new int[8];
+
+for (int i = 0; i < arrayA.Length; i++)
+{
+    arrayA[i] = new Random().Next(0, 2);
+    Console.WriteLine("Элемент массива {0} равен {1}", i+1, arrayA[i]);
+}
