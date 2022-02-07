225d38633ecdd2f452a9b286f73be2e1a5fa47b7
Eugene <eugene_el@mail.ru>
2022-02-07 19:01:37 +0700

    ex 026 done

---
+++ 002_junior/026_power/Program.cs
@@ -0,0 +1,18 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите основание степени А");
+int baseNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите показатель степени В");
+int indexNum = int.Parse(Console.ReadLine());
+
+int sum = 1;
+string sumStr;
+
+for (int i = 1; i <= Math.Abs(indexNum); i++) 
+{
+    sum *= baseNum;
+}
+sumStr = sum.ToString();
+if (indexNum < 0) sumStr = "1/" + sumStr;       // for negative indexes
+
+Console.WriteLine("Число {0} в степени {1} равно {2}", baseNum, indexNum, sumStr);
