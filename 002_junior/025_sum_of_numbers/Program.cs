475269d37edd3f6f0c3fa109aa001e84c5217118
Eugene <eugene_el@mail.ru>
2022-02-07 17:53:25 +0700

    ex 025 done

---
+++ 002_junior/025_sum_of_numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите положительное целое число А");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректно заданное значение");
+    return;
+}
+
+int sum = 0;
+
+for (int i = 1; i <= number; i++) sum += i;
+Console.WriteLine("Сумма чисел от 1 до {0} равна {1}", number, sum);
