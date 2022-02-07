c84fe2db9056012d4509b0ce7a4ce05289147122
Eugene <eugene_el@mail.ru>
2022-02-07 20:00:58 +0700

    ex 028 done

---
+++ 002_junior/028_digits_sum/Program.cs
@@ -0,0 +1,16 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите целое число");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+
+Console.Write("В числе {0}", number);
+number = Math.Abs(number);
+
+while (number > 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+
+Console.WriteLine(" сумма значений разрядов равна {0}", sum);
