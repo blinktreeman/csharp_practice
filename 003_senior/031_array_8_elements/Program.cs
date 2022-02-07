71c767be4e63260e6603ae1058e0741aefee2287
Eugene <eugene_el@mail.ru>
2022-02-07 21:08:48 +0700

    ex 031 done

---
+++ 003_senior/031_array_8_elements/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int[] arrayA = new int[8] {1, 2, 3, 4, 5, 6, 7, 8};
+
+for (int i = 0; i < arrayA.Length; i++)
+{
+    Console.WriteLine("Элемент массива {0} равен {1}", i+1, arrayA[i]);
+}
