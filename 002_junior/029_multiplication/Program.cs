e6cbd9342c03f1e68ee16ecb40cea2b86c85a6bd
Eugene <eugene_el@mail.ru>
2022-02-07 20:15:01 +0700

    ex 029 done

---
+++ 002_junior/029_multiplication/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите положительное целое число N");
+int number = int.Parse(Console.ReadLine());
+int multiRes = 1;
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректно заданное значение");
+    return;
+}
+
+for (int i = 1; i <= number; i++)
+{
+    multiRes *= i;
+}
+
+Console.WriteLine("Произведение чисел от 1 до {0} (N!) равно {1}", number, multiRes);
