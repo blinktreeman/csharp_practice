2a6320e8cae76767e101e46a5ecbb907d2b62c88
Eugene <eugene_el@mail.ru>
2022-02-07 17:30:32 +0700

    ex 023 done

---
+++ 002_junior/023_squares/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите целое число N");
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Таблица квадратов чисел от 1 до N");
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine("{0, 5}^2 = {1, -20}", i, i*i);
+}
