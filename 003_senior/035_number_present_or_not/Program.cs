5962bae0e65af3ca51230541bcb396bc0da4f13d
Eugene <eugene_el@mail.ru>
2022-02-07 22:14:43 +0700

    ex 035 done

---
+++ 003_senior/035_number_present_or_not/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] arrayA = new int[8] {1, 2, 3, 4, 5, 6, 7, 8};
+
+Console.WriteLine("Введите целое число");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < arrayA.Length; i++)
+{
+    if (arrayA[i] == number)
+    {
+        Console.WriteLine("Число {0} присутствует в массиве", number);
+        return;
+    }
+}
+
+Console.WriteLine("Число {0} отсутствует в массиве", number);
