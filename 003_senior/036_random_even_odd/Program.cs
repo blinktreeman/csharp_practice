e6acd77284a62691b3f683dd3ff8891f128ea8d8
Eugene <eugene_el@mail.ru>
2022-02-07 22:31:06 +0700

    ex 036 done

---
+++ 003_senior/036_random_even_odd/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] arrayA = new int[30];
+int evenNum = 0;
+int oddNum = 0;
+
+Console.WriteLine("Массив с числами:");
+
+for (int i = 0; i < arrayA.Length; i++)
+{
+    arrayA[i] = new Random().Next(100, 1000);
+    if (arrayA[i] % 2 == 0) evenNum++;
+    else oddNum++;
+    Console.Write(arrayA[i] + " ");
+}
+
+Console.WriteLine("\nКоличество четных чисел в массиве {0}", evenNum);
+Console.WriteLine("Количество нечетных чисел в массиве {0}", oddNum);
