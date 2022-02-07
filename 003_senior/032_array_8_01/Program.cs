b5f7fa153ba23b47781959c86484ed3f455695c9
Eugene <eugene_el@mail.ru>
2022-02-07 22:57:48 +0700

    correct random

--- 003_senior/032_array_8_01/Program.cs
+++ 003_senior/032_array_8_01/Program.cs
@@ -1,9 +1,10 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 
 int[] arrayA = new int[8];
+Random rand = new Random();
 
 for (int i = 0; i < arrayA.Length; i++)
 {
-    arrayA[i] = new Random().Next(0, 2);
+    arrayA[i] = rand.Next(0, 2);
     Console.WriteLine("Элемент массива {0} равен {1}", i+1, arrayA[i]);
 }
