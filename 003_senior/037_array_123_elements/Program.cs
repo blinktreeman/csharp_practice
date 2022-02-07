5732041f640df20caa5b27a544f7dfebf3dc6391
Eugene <eugene_el@mail.ru>
2022-02-07 22:52:50 +0700

    ex 037 done

---
+++ 003_senior/037_array_123_elements/Program.cs
@@ -0,0 +1,20 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] arrayA = new int[123];
+Random rand = new Random();
+
+Console.WriteLine("Задан массив со следующими элементами:");
+for (int i = 0; i < arrayA.Length; i++)
+{
+    arrayA[i] = rand.Next(0, 1000);
+    Console.Write(arrayA[i] + " ");
+}
+
+int count = 0;
+
+for (int i = 0; i < arrayA.Length; i++)
+{
+    if (arrayA[i] >= 10 & arrayA[i] <= 99) count++;
+}
+
+Console.WriteLine("\n"+"В массиве {0} элементов принадлежащих интервалу [10,99]", count);
