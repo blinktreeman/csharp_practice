8624246e53ca317a13dbee6a62f6bff45a740ef6
Eugene <eugene_el@mail.ru>
2022-02-07 19:18:44 +0700

    ex 027 done

---
+++ 002_junior/027_how_many_digits/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите целое число");
+string numStr = Console.ReadLine();
+
+Console.WriteLine("В числе {0} разрядов {1}", numStr, numStr.Length);
+
+int number = int.Parse(numStr);
+int numDigits = 0;
+
+while (number > 0)
+{
+    number /= 10;
+    numDigits ++;
+}
+
+Console.WriteLine("Точно, разрядов {0}", numDigits);
