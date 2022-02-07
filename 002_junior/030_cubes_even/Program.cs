7eef2f3d1d8f08c8f2f22cde778defa576e161e2
Eugene <eugene_el@mail.ru>
2022-02-07 20:29:15 +0700

    ex 030 done

---
+++ 002_junior/030_cubes_even/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Задайте диапазон целых чисел");
+Console.Write("Начало диапазона А=");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Конец диапазона B=");
+int numB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов чисел от A до B заканчивающихся на четную цифру");
+for (int i = numA; i <= numB; i++)
+{
+    if (i % 2 == 0) Console.WriteLine("{0, 5}^3 = {1, -20}", i, Math.Pow(i, 3));
+}
