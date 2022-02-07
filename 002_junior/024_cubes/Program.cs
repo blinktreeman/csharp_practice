29e894587735f2a610234b5e269e3c0f6197daf9
Eugene <eugene_el@mail.ru>
2022-02-07 17:36:39 +0700

    ex 024 done

---
+++ 002_junior/024_cubes/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите целое число N");
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов чисел от 1 до N");
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine("{0, 5}^3 = {1, -20}", i, Math.Pow(i, 3));
+}
