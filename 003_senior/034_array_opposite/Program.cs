58c9e25720b6b7a98e08720e4cab2a7a35c0401d
Eugene <eugene_el@mail.ru>
2022-02-07 21:54:52 +0700

    ex 034 done

---
+++ 003_senior/034_array_opposite/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+int[] arrayA = new int[8] {1, 2, 3, 4, 5, 6, 7, 8};
+
+Console.WriteLine("Исходный массив");
+for (int i = 0; i < arrayA.Length; i++)
+{
+    Console.Write(arrayA[i] + " ");
+}
+
+Console.WriteLine("");
+Console.WriteLine("Элементы заменены на противоположные");
+
+for (int i = 0; i < arrayA.Length / 2; i++)
+{
+    int temp = arrayA[i];
+    arrayA[i] = arrayA[^(i+1)];
+    arrayA[^(i+1)] = temp;
+}
+
+for (int i = 0; i < arrayA.Length; i++)
+{
+    Console.Write(arrayA[i] + " ");
+}
