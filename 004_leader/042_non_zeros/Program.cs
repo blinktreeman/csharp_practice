409494fb85b6382741b80da34a491030d409bff9
Eugene <eugene_el@mail.ru>
2022-02-08 21:03:28 +0700

    ex 042 done

---
+++ 004_leader/042_non_zeros/Program.cs
@@ -0,0 +1,24 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+double number = 0;
+int aboveZero = 0;
+
+Console.WriteLine("Поочередно вводите числа. Для выхода - любое нечисловое значение");
+
+do
+{
+    try
+    {
+        number = double.Parse(Console.ReadLine());
+    }
+    catch
+    {
+        Console.WriteLine("Введено {0} чисел больше нуля", aboveZero);
+        return;       
+    }
+    finally
+    {
+        if (number > 0) aboveZero++;
+    }
+}
+while (true);
