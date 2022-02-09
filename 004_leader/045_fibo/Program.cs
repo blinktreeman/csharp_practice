af191fe0c6d228d4e1a748672dc216ad45a07fc6
Eugene <eugene_el@mail.ru>
2022-02-09 22:52:42 +0700

    ex 045 done

---
+++ 004_leader/045_fibo/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа Фибоначчи
+void showFibo (int last, int current, int limOut)
+{
+    if (current + last > limOut) return;
+    Console.Write("{0} ", last + current);
+    showFibo (current, last + current, limOut);
+}
+
+Console.WriteLine("Задайте предел для числовой последовательности Фибоначчи");
+
+int limit = int.Parse(Console.ReadLine());
+
+Console.Write("0 1 1 ");
+showFibo (1, 1, limit);
