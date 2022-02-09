d2c588c3aa7557265f357f73dfda514114509297
Eugene <eugene_el@mail.ru>
2022-02-09 22:22:50 +0700

    ex 044 done

---
+++ 004_leader/044_cross_lines/Program.cs
@@ -0,0 +1,31 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+double  k1 = 3, k2 = 5, 
+        b1 = 2, b2 = 8,
+        xCross = 0, yCross = 0;
+
+/* 
+y1 = k1 * x + b1
+y2 = k2 * x + b2
+В точке пересечения y1 = y2, x1 = x2
+k1 * x + b1 = k2 * x + b2, отсюда:
+k1 * x - k2 * x = b2 - b1
+x = (b2 - b1) / (k1 - k2)
+*/
+
+Console.WriteLine("Точка персечения прямых");
+Console.WriteLine("1. y = {0} * x + {1}", k1, b1);
+Console.WriteLine("2. y = {0} * x + {1}", k2, b2);
+
+if (k1 == k2) 
+{
+    Console.WriteLine("Прямые не пересекаются");
+    return;
+}
+
+xCross = (b2 - b1) / (k1 - k2);
+
+// B из первого уравнения:
+yCross = k1 * xCross + b1;
+
+Console.WriteLine("есть точка с координатами ({0}, {1})", xCross, yCross);
