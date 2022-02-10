92297cce8007d3399513fd829fdbc6a174671df0
Eugene <eugene_el@mail.ru>
2022-02-11 01:15:45 +0700

    change task

--- 002_junior/027_how_many_digits/Program.cs
+++ 002_junior/027_how_many_digits/Program.cs
@@ -1,6 +1,6 @@
-﻿// Определить количество цифр в числе
+﻿// Определить количество цифр в слове (числе)
 
-Console.WriteLine("Введите целое число");
+/*Console.WriteLine("Введите целое число");
 int number = int.Parse(Console.ReadLine());
 int numDigits = 1;
 
@@ -8,8 +8,28 @@ Console.Write("В числе {0}", number);
 
 while (Math.Abs(number) > 9)
 {
-    number /= 10;
+    number /= 10;       // 123456
     numDigits ++;
 }
 
 Console.WriteLine(" разрядов {0}", numDigits);
+*/
+
+Console.WriteLine("Введите последовательность знаков");
+string strA = Console.ReadLine();
+
+//int[] arrayA = new int[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int count = 0;
+int number;
+
+for (int i = 0; i < strA.Length; i++)
+{
+    if (int.TryParse(strA[i].ToString(), out number)) count++;
+}
+Console.WriteLine($"Количество цифр {count}");
+
+//foreach (strA) 
+//{
+//    int.TryParse(strA)
+//}
