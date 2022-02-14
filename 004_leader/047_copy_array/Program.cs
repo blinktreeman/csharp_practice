7b5ad66a9118f5bfd846f70f666d90121952c62a
Eugene <eugene_el@mail.ru>
2022-02-14 19:43:07 +0700

    ex 047 done

---
+++ 004_leader/047_copy_array/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу копирования массива
+
+Console.WriteLine("Исходный массив");
+int[] arrayA = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+Console.WriteLine(string.Join(" ", arrayA));
+
+int[] arrayB = new int[arrayA.Length];
+
+// arrayB = arrayA;
+
+for (int i=0; i < arrayA.Length; i++)
+{
+    arrayB[i] = arrayA[i];
+}
+
+Console.WriteLine("Результат копирования");
+Console.WriteLine(string.Join(" ", arrayB));
