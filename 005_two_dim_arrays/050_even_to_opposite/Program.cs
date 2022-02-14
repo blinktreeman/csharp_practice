20d273442a9f03c076b40d9bda89485cdfacbefa
Eugene <eugene_el@mail.ru>
2022-02-14 22:37:57 +0700

    ex 050 done

---
+++ 005_two_dim_arrays/050_even_to_opposite/Program.cs
@@ -0,0 +1,59 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные (?)
+
+int[,] makeArray (int rws, int cols)    // Generate array
+{
+    Random rand = new Random();
+    int[,] tempArray = new int[rws, cols];
+
+    for (int i = 0; i < rws; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            tempArray[i, j] = rand.Next(0, 100); 
+        }
+    }
+    return tempArray;
+}
+
+void showArray (int[,] tempArr)          //Show array
+{
+    for (int i = 0; i < tempArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArr.GetLength(1); j++)
+        {
+            Console.Write($"{tempArr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] changeElements (int[,] chArray)
+{
+    for (int i = 0; i < chArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < chArray.GetLength(1) / 2; j++)
+        {
+            if (j % 2 != 0) 
+            {
+                int tempNum = chArray[i, j];
+                chArray[i, j] = chArray[i, chArray.GetLength(1)-(j+1)];
+                chArray[i, chArray.GetLength(1)-(j+1)] = tempNum;
+            }
+        }
+    }
+    return (chArray);
+}
+
+Console.WriteLine("Задайте размер двумерного массива");
+Console.Write("строки n=");
+int rows =  int.Parse(Console.ReadLine());
+Console.Write("столбцы k=");
+int columns =  int.Parse(Console.ReadLine());
+
+int[,] arrayA = makeArray(rows, columns);
+
+Console.WriteLine("Исходный массив");
+showArray(arrayA);
+changeElements(arrayA);
+Console.WriteLine("Измененный массив");
+showArray(arrayA);
