7164a5761050e67124b56b11f68a5e1073ce8ddd
Eugene <eugene_el@mail.ru>
2022-02-14 20:56:49 +0700

    ex 048 done

---
+++ 005_two_dim_arrays/048_two_dim_of_integers/Program.cs
@@ -0,0 +1,38 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+int[,] makeArray (int rws, int cols)
+{
+    Random rand = new Random();
+    int[,] tempArray = new int[rws, cols];
+
+    for (int i = 0; i < rws; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            tempArray[i, j] = rand.Next(0, 100); 
+        }
+    }
+    return tempArray;
+}
+
+void showArray(int[,] tempArr)
+{
+    for (int i = 0; i < tempArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArr.GetLength(1); j++)
+        {
+            Console.Write($"{tempArr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Задайте размер двумерного массива");
+Console.Write("строки m=");
+int rows =  int.Parse(Console.ReadLine());
+Console.Write("столбцы n=");
+int columns =  int.Parse(Console.ReadLine());
+
+int[,] arrayA = makeArray(rows, columns);
+
+showArray(arrayA);
