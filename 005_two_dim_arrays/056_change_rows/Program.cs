09aa253bb2bba6f5b30cdf64ba2be43117bf6460
Eugene <eugene_el@mail.ru>
2022-02-15 12:32:04 +0700

    ex 56 done

---
+++ 005_two_dim_arrays/056_change_rows/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] makeArray (int rws, int cols)
+{
+    Random rand = new Random();
+    int[,] tempArray = new int[rws, cols];
+
+    for (int i = 0; i < rws; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            tempArray[i, j] = rand.Next(0, 10); 
+        }
+    }
+    return tempArray;
+}
+
+void showArray(int[,] tempArr)
+{
+    for (int i = 0; i < tempArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArr.GetLength(1); j++)
+        {
+            Console.Write($"{tempArr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arrayA = makeArray(5, 6);
+Console.WriteLine("Исходный массив");
+showArray(arrayA);
+
+for (int j = 0; j < arrayA.GetLength(1); j++)
+{
+    int temp = arrayA[0, j];
+    arrayA[0, j] = arrayA[arrayA.GetLength(0) - 1, j];
+    arrayA[arrayA.GetLength(0) - 1, j] = temp;
+}
+
+Console.WriteLine("Полученный массив");
+showArray(arrayA);
