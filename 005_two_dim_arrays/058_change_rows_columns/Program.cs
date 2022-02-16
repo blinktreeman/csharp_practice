e8e19a06b04ba8ee043e1783d4f0911c925cc13d
Eugene <eugene_el@mail.ru>
2022-02-16 21:54:04 +0700

    change array name

--- 005_two_dim_arrays/058_change_rows_columns/Program.cs
+++ 005_two_dim_arrays/058_change_rows_columns/Program.cs
@@ -16,13 +16,13 @@ int[,] FillArray (int rowsToFill, int columnsToFill)
     return tempArray;
 }
 
-void ShowArray(int[,] tempArr)
+void ShowArray(int[,] inputArray)
 {
-    for (int i = 0; i < tempArr.GetLength(0); i++)
+    for (int i = 0; i < inputArray.GetLength(0); i++)
     {
-        for (int j = 0; j < tempArr.GetLength(1); j++)
+        for (int j = 0; j < inputArray.GetLength(1); j++)
         {
-            Console.Write($"{tempArr[i,j]} ");
+            Console.Write($"{inputArray[i,j]} ");
         }
         Console.WriteLine();
     }
