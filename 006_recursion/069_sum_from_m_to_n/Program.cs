c97f8fab0888a9bde5adb540da82ceb5698f630f
Eugene <eugene_el@mail.ru>
2022-02-17 21:31:58 +0700

    ex 069 done

---
+++ 006_recursion/069_sum_from_m_to_n/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumOfNumbers (int fromNumber, int toNumber)
+{
+    if (fromNumber == toNumber) return fromNumber;
+    else return fromNumber + SumOfNumbers(++fromNumber, toNumber);
+}
+
+Console.WriteLine("Задайте натуральное число");
+Console.Write("N=");
+int beginNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте натуральное число");
+Console.Write("M=");
+int endNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Сумма чисел от N до M");
+
+Console.WriteLine(SumOfNumbers(beginNumber, endNumber));
