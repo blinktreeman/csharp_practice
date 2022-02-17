2039f1522e14af3853c17e8f64a75c7d95ff2a92
Eugene <eugene_el@mail.ru>
2022-02-17 20:54:59 +0700

    ex 066 done

---
+++ 006_recursion/066_from_1_to_n/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+string naturalNumbers(int fromNumber, int endOfInterval)
+{
+    if (fromNumber == endOfInterval) return fromNumber.ToString() + " ";
+    else return fromNumber.ToString() + " " + naturalNumbers(++fromNumber, endOfInterval);
+}
+
+Console.WriteLine("Задайте положительное натуральное число");
+Console.Write("N=");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Натуральные числа от 1 до N");
+Console.WriteLine(naturalNumbers(1, number));
