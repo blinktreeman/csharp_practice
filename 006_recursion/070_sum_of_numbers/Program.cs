76909a54a319502f8918a69f28f473011736845e
Eugene <eugene_el@mail.ru>
2022-02-17 22:49:58 +0700

    ex 070 done

---
+++ 006_recursion/070_sum_of_numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму цифр числа
+
+int SumOfNumbers (int inputNumber)
+{
+    if (inputNumber < 10) return inputNumber;
+    else return inputNumber % 10 + SumOfNumbers(inputNumber / 10);  
+}
+
+
+Console.WriteLine("Введите целое число");
+int number = int.Parse(Console.ReadLine());
+if (number < 0) number = -number;
+
+Console.WriteLine($"Сумма цифр числа равна {SumOfNumbers(number)}");
