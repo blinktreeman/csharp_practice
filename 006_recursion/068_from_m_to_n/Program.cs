ffb3dc1af87c9b3ffb50f79001ce0ed6c12e3cbc
Eugene <eugene_el@mail.ru>
2022-02-17 21:23:05 +0700

    ex 068 done

---
+++ 006_recursion/068_from_m_to_n/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+string NaturalNumbers(int number, int endOfInterval)
+{
+    if (number == endOfInterval) return number.ToString();
+    else return number.ToString() + " " + NaturalNumbers(++number, endOfInterval);
+}
+
+
+
+Console.WriteLine("Задайте натуральное число");
+Console.Write("N=");
+int beginNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте натуральное число");
+Console.Write("M=");
+int endNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Натуральные числа от N до M");
+
+Console.WriteLine(NaturalNumbers(beginNumber, endNumber));
