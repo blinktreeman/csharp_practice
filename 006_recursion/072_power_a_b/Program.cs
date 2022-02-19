ad2e25d6617404c76db748b153459a78e92d2fa7
Eugene <eugene_el@mail.ru>
2022-02-19 13:37:59 +0700

    ex 072 done, but not for negative indexes

---
+++ 006_recursion/072_power_a_b/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int PowerAB(int baseNum, int indexNum)
+{
+    if (indexNum == 1) return baseNum;
+    else return baseNum * PowerAB(baseNum, --indexNum);
+}
+
+Console.WriteLine("Задайте основание степени");
+Console.Write("A=");
+int baseNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте показатель степени");
+Console.Write("B=");
+int indexNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Число {baseNumber} в степени {indexNumber} равно {PowerAB(baseNumber, indexNumber)}");
