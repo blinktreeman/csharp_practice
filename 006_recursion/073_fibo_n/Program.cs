47a9ccdd1a18a8ebac57becb74c90cbff010e119
Eugene <eugene_el@mail.ru>
2022-02-19 14:11:02 +0700

    ex 073 done

---
+++ 006_recursion/073_fibo_n/Program.cs
@@ -0,0 +1,27 @@
+﻿// Написать программу показывающие первые N чисел, 
+// для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+void ShowNumbers(int firstNum, int secondNum, int nums)
+{
+    if (nums == 0) return;
+    else
+    {
+        Console.Write($"{firstNum + secondNum} ");
+        ShowNumbers(secondNum, firstNum + secondNum, --nums);
+    } 
+    
+}
+
+Console.WriteLine("Задайте первое число последовательности");
+Console.Write("A=");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте второе число последовательности");
+Console.Write("B=");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте количество чисел последовательности для вывода");
+Console.Write("N=");
+int numbers = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Числовая последовательность ");
+ShowNumbers(firstNumber, secondNumber, numbers);
