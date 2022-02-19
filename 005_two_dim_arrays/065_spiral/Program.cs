9a15e4be27c7c0b213e8b291f6081e88f51bab9d
Eugene <eugene_el@mail.ru>
2022-02-20 03:57:20 +0700

    ex 065 done, but...

---
+++ 005_two_dim_arrays/065_spiral/Program.cs
@@ -0,0 +1,69 @@
+﻿// Спирально заполнить двумерный массив:
+// 1  2  3  4
+// 12 13 14  5
+// 11 16 15  6
+// 10  9  8  7
+
+int[,] RotateArray(int[,] inputArray)
+{
+    // Transpose matrix
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = i + 1; j < inputArray.GetLength(1); j++)
+        {
+            int temp = inputArray[i, j];
+            inputArray[i, j] = inputArray[j, i];
+            inputArray[j, i] = temp;
+        }
+    }
+    // Mirror horizontally
+    for (int i = 0; i < inputArray.GetLength(0) / 2; i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            int temp = inputArray[i, j];
+            inputArray[i, j] = inputArray[inputArray.GetLength(0) - 1 - i, j];
+            inputArray[inputArray.GetLength(0) - 1 - i, j] = temp;
+        }
+    }
+    return inputArray;
+} 
+
+void ShowArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write($"{inputArray[i,j], -3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] spiralArray = new int[4, 4];
+
+int indexI = 0, 
+    indexJ = 0, 
+    rotates = 0;
+
+for (int i = 1; i <= spiralArray.GetLength(0) * spiralArray.GetLength(1); i++)
+{
+    if (indexJ < spiralArray.GetLength(1) && spiralArray[indexI, indexJ] == 0)
+    {
+        spiralArray[indexI, indexJ] = i;
+        indexJ++;
+    }
+    else 
+    {
+        RotateArray(spiralArray);
+        rotates++;
+        indexI = rotates / 4;
+        indexJ = rotates / 5 + 1;
+        spiralArray[indexI, indexJ] = i;
+        indexJ++;
+    }
+}
+
+while (spiralArray[0, 0] != 1) RotateArray(spiralArray);
+ShowArray(spiralArray);
