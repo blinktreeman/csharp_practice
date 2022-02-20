1681fd2a355b08eda1eaa0e934b2447e79f39eec
Eugene <eugene_el@mail.ru>
2022-02-20 23:38:44 +0700

    remake for

--- 007_hell_or_not/076_relatively_prime_numbers/Program.cs
+++ 007_hell_or_not/076_relatively_prime_numbers/Program.cs
@@ -6,20 +6,15 @@ Console.WriteLine("Задайте натуральное число N (N <= 10^2
 ulong limit = ulong.Parse(Console.ReadLine());
 
 Console.WriteLine("Число М групп взаимно простых чисел при разбиении");
-Console.WriteLine("чисел от 1 до N равно");
+Console.Write("чисел от 1 до N равно ");
 double groups = Math.Floor(Math.Log2(limit)+1);
 Console.WriteLine(groups);
 
 Console.WriteLine("Группы чисел");
-int index = 0;
-for (ulong i = 1; i <= limit; i++)
+for (int i = 0; i < groups; i++)
 {
-    Console.Write($"Группа {index + 1}: ");
-    for (ulong j = i; (j < Convert.ToUInt64(Math.Pow(2, index+1)) && j <= limit); j++)
-    {
+    Console.Write($"Группа {i + 1}: ");
+    for (ulong j = Convert.ToUInt64(Math.Pow(2, i)); (j < Convert.ToUInt64(Math.Pow(2, i+1)) && j <= limit); j++)
         Console.Write($"{j} ");
-        i = j;
-    }
     Console.WriteLine();
-    index++;
 }
